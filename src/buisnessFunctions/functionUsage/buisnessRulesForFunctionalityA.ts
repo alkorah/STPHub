eef57090b5a5509d28e8e203a0a474ca7ba2feca
Magnus Reeves <rreeves8@uwo.ca>
2024-06-21 16:07:08 -0400

    spit rules away

--- src/buisnessFunctions/functionUsage/buisnessRulesForFunctionalityA.ts
+++ src/buisnessFunctions/functionUsage/buisnessRulesForFunctionalityA.ts
@@ -17,6 +17,11 @@ type BuissnessRuleBResult = {
   e: number;
 };
 
+const rules = {
+  ruleA: ({ b, c }: Payload): BuisnessRuleAResult => ({ d: b + c }),
+  ruleB: ({ b, c }: Payload): BuissnessRuleBResult => ({ e: b - c }),
+};
+
 //the result, can place this in response builder generic
 export type Result = BuisnessRuleAResult | BuissnessRuleBResult;
 
@@ -28,15 +33,11 @@ export const buisnessRulesForFunctionalityA = new BuisnessLogicBuilder<
   handler(payload) {
     //buisness rule 1
     if (payload.a === "rule A, returns type A result") {
-      return {
-        d: payload.b + payload.c,
-      };
+      return rules.ruleA(payload);
     }
     //buisness rule 2
     else if (payload.a === "rule B, returns type B result") {
-      return {
-        e: payload.b - payload.c,
-      };
+      return rules.ruleB(payload);
     } else {
       throw new BuisnessLogicException("No rule matched");
     }
